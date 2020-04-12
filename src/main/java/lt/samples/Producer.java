9b6fbdea84a9f6f352498d7400dcc397d907c1f2
bmonteiro00 <bmonteiro00@gmail.com>
2020-04-12 01:32:55 -0300

    Refactor PipeMovie stream

--- src/main/java/lt/samples/Producer.java
+++ src/main/java/lt/samples/Producer.java
@@ -1,18 +1,15 @@
 package lt.samples;
 
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerializer;
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import lt.serializers.MovieSerializer;
 import lt.avro.Movie;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.ByteArraySerializer;
-import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.StreamsConfig;
 
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectOutputStream;
 import java.util.Properties;
 
 
@@ -30,46 +27,35 @@ class Name extends AbstractKafkaAvroSerializer{
 
 public class Producer  {
 
-
     public static void main(String[] args) {
 
         Properties properties=new Properties();
         properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "LT-streams");
         properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
-        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                ByteArraySerializer.class);
+               ByteArraySerializer.class);
 
-        KafkaProducer<String, byte[]> myProducer= new KafkaProducer<String, byte[]>(properties);
+        KafkaProducer<String, byte[]> myProducer = new KafkaProducer<String, byte[]>(properties);
 
-        try {
+        for(int i=1;i<10;i++){
 
-            for(int i=1;i<10;i++){
+            Movie movie = new Movie().newBuilder().setMovieId(i).setReleaseYear(1998).setTitle("Title" + String.valueOf(i)).build();
+            MovieSerializer v = new MovieSerializer();
 
-                Movie movie = new Movie();
-                movie.setMovieId(Long.valueOf(i));
-                movie.setReleaseYear(Integer.valueOf(i));
-                movie.setTitle("ddddddd");
+//            GenericData.Record record = new GenericData.Record(movie.getClassSchema());
+//            record.put("movie_id", movie.getMovieId());
+//            record.put("release_year", movie.getReleaseYear());
+//            record.put("title", movie.getTitle());
+//            byte[] serdes1 = v.serialize("streams-lt-input", record.);
+//            myProducer.send(new  ProducerRecord<String, byte[]>("streams-lt-input", KafkaProducer.class.getName() , record.toString().getBytes()));
 
-                Name name = new Name();
-                name.setName(String.valueOf(i));
 
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                ObjectOutputStream oos = new ObjectOutputStream(bos);
-                oos.writeObject(movie);
-                oos.flush();
-                byte [] data = bos.toByteArray();
-
-                myProducer.send(new  ProducerRecord<String, byte[]>("streams-lt-input", KafkaProducer.class.getName() , data));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally{
-            myProducer.close();
+            byte[] serdes2 = v.serialize("streams-lt-input", movie);
+            myProducer.send(new  ProducerRecord<String, byte[]>("streams-lt-input", KafkaProducer.class.getName() , serdes2));
         }
+
     }
 
 }
